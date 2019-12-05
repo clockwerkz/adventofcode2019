4b8fc96376ebbafe27f523e473b93115fd397bf4
clockwerkz <clockwerkz@gmail.com>
2019-12-05 00:00:56 -0800

    solved second part of day4!

--- day4 - secure container/one.js
+++ day4 - secure container/one.js
@@ -1,8 +1,8 @@
 const validateNumber = require('./validateNumber');
 
-// console.log(validateNumber(112233));
-// console.log(validateNumber(123444));
-// console.log(validateNumber(111122));
+console.log(validateNumber(166888), true);
+console.log(validateNumber(123444), false);
+console.log(validateNumber(111122), true);
 let total = 0;
 
 for (let i=168630; i<718098; i++) {
--- day4 - secure container/validateNumber.js
+++ day4 - secure container/validateNumber.js
@@ -1,23 +1,22 @@
 module.exports = function (num) {
-    if (num < 100000 || num > 999999 ) return false;
-    let divider = 100000;
-    let lastNumber = Math.floor(num / divider);
-    num = num % divider;
+    const arr = num.toString().split('').map(el => parseInt(el));
+    if (arr.length !==6) return false;
+    let lastNumber = arr[0];
     let matchingPair = false;
-    divider /= 10;
     let sameNumberCount = 1;
-    while (divider >= 1) {
-        let currentNumber = Math.floor(num / divider);
-        num = num % divider;
+    for (let i=1; i<arr.length; i++) {
+        let currentNumber = arr[i];
         if (currentNumber < lastNumber) return false;
-        if (currentNumber === lastNumber && !matchingPair) { 
-            sameNumberCount++;
+        if (currentNumber === lastNumber) {
+            if (!matchingPair) { 
+                sameNumberCount++;
+            }
         } else {
-            matchingPair = sameNumberCount === 2;
+            if (sameNumberCount===2) matchingPair = true;
             sameNumberCount=1;
         }
         lastNumber = currentNumber;
-        divider /= 10;
     }
-    return (sameNumberCount === 2 && !matchingPair) ? true : matchingPair;
+    if (sameNumberCount===2) matchingPair = true;
+    return matchingPair;
 }
